643cf6f0f9dba440da88764e7ab1581967f86e66
J Fixby <github@jfixby.com>
2019-08-12 21:21:03 +0200

    reject transactions below wire.DefaultBlockVersion

--- txscript/opcode.go
+++ txscript/opcode.go
@@ -1246,7 +1246,7 @@ func opcodeCheckSequenceVerify(op *parsedOpcode, vm *Engine) error {
 
 	// Transaction version numbers not high enough to trigger CSV rules must
 	// fail.
-	if vm.tx.Version < 2 {
+	if vm.tx.Version < wire.DefaultBlockVersion {
 		str := fmt.Sprintf("invalid transaction version: %d",
 			vm.tx.Version)
 		return scriptError(ErrUnsatisfiedLockTime, str)
