0ff2a9ee862d140fa09fb1c2db50530bb375b836
J Fixby <github@jfixby.com>
2019-09-03 13:59:19 +0200

    TestMainExecution

---
+++ main_test.go
@@ -0,0 +1,48 @@
+package main
+
+import (
+	"fmt"
+	"github.com/picfight/pfcd/limits"
+	"os"
+	"runtime"
+	"runtime/debug"
+	"testing"
+)
+
+func TestMainExecution(t *testing.T) {
+	t.Skip()
+	// Use all processor cores.
+	runtime.GOMAXPROCS(runtime.NumCPU())
+
+	// Block and transaction processing can cause bursty allocations.  This
+	// limits the garbage collector from excessively overallocating during
+	// bursts.  This value was arrived at with the help of profiling live
+	// usage.
+	debug.SetGCPercent(10)
+
+	// Up some limits.
+	if err := limits.SetLimits(); err != nil {
+		fmt.Fprintf(os.Stderr, "failed to set limits: %v\n", err)
+		os.Exit(1)
+	}
+
+	// Call serviceMain on Windows to handle running as a service.  When
+	// the return isService flag is true, exit now since we ran as a
+	// service.  Otherwise, just fall through to normal operation.
+	if runtime.GOOS == "windows" {
+		isService, err := winServiceMain()
+		if err != nil {
+			fmt.Println(err)
+			os.Exit(1)
+		}
+		if isService {
+			os.Exit(0)
+		}
+	}
+
+	// Work around defer not working after os.Exit()
+	if err := btcdMain(nil); err != nil {
+		os.Exit(1)
+	}
+
+}
