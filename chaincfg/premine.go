ff49b76c0e849c2de60373039a7528c024c34ce5
J Fixby <github@jfixby.com>
2019-09-07 18:08:19 +0200

    empty premine

--- chaincfg/premine.go
+++ chaincfg/premine.go
@@ -10,25 +10,14 @@ package chaincfg
 var BlockOneLedgerMainNet = []*TokenPayout{}
 
 // BlockOneLedgerTestNet3 is the block one output ledger for testnet version 3.
-var BlockOneLedgerTestNet3 = []*TokenPayout{
-	{"Tsi6gGYNSMmFwi7JoL5Li39SrERZTTMu6vY", 80000 * 1e8},
-	{"TscB7V5RuR1oXpA364DFEsNDuAs8Rk6BHJE", 20000 * 1e8},
-}
+var BlockOneLedgerTestNet3 = []*TokenPayout{}
 
 // BlockOneLedgerSimNet is the block one output ledger for the simulation
 // network.  See "PicFight organization related parameters" in simnetparams.go for
 // information on how to spend these outputs.
-var BlockOneLedgerSimNet = []*TokenPayout{
-	{"Sshw6S86G2bV6W32cbc7EhtFy8f93rU6pae", 100000 * 1e8},
-	{"SsjXRK6Xz6CFuBt6PugBvrkdAa4xGbcZ18w", 100000 * 1e8},
-	{"SsfXiYkYkCoo31CuVQw428N6wWKus2ZEw5X", 100000 * 1e8},
-}
+var BlockOneLedgerSimNet = []*TokenPayout{}
 
 // BlockOneLedgerRegNet is the block one output ledger for the regression test
 // network.  See "PicFight organization related parameters" in regnetparams.go for
 // information on how to spend these outputs.
-var BlockOneLedgerRegNet = []*TokenPayout{
-	{"RsKrWb7Vny1jnzL1sDLgKTAteh9RZcRr5g6", 100000 * 1e8},
-	{"Rs8ca5cDALtsMVD4PV3xvFTC7dmuU1juvLv", 100000 * 1e8},
-	{"RsHzbGt6YajuHpurtpqXXHz57LmYZK8w9tX", 100000 * 1e8},
-}
+var BlockOneLedgerRegNet = []*TokenPayout{}
