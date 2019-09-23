38d8e28ba09d39cd774a5c17edc7b55d7dcdd625
J Fixby <github@jfixby.com>
2019-09-23 22:42:17 +0200

    picFightCoinNetParams

--- params.go
+++ params.go
@@ -11,7 +11,7 @@ import (
 
 // activeNetParams is a pointer to the parameters specific to the
 // currently active Decred network.
-var activeNetParams = &mainNetParams
+var activeNetParams = &picFightCoinNetParams
 
 // params is used to group parameters for various networks such as the main
 // network and test networks.
@@ -20,13 +20,13 @@ type params struct {
 	rpcPort string
 }
 
-// mainNetParams contains parameters specific to the main network
+// picFightCoinNetParams contains parameters specific to the main network
 // (wire.MainNet).  NOTE: The RPC port is intentionally different than the
 // reference implementation because dcrd does not handle wallet requests.  The
 // separate wallet process listens on the well-known port and forwards requests
 // it does not handle on to dcrd.  This approach allows the wallet process
 // to emulate the full reference implementation RPC API.
-var mainNetParams = params{
+var picFightCoinNetParams = params{
 	Params:  &chaincfg.PicFightCoinParams,
 	rpcPort: "9109",
 }
