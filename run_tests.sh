184e21a3b4410f856bc7236b9a4e2401d078687d
J Fixby <github@jfixby.com>
2019-09-24 23:31:11 +0200

    $GO test -v ./...

--- run_tests.sh
+++ run_tests.sh
@@ -38,28 +38,8 @@ testrepo () {
 
   $GO version
   $GO clean -testcache
-  # binary needed for RPC tests
-  env CC=gcc $GO build
-  cp "$REPO" "$GOPATH/bin/"
-
-  # run tests on all modules
-  ROOTPATH=$($GO list -m -f {{.Dir}} 2>/dev/null)
-  ROOTPATHPATTERN=$(echo $ROOTPATH | sed 's/\\/\\\\/g' | sed 's/\//\\\//g')
-  MODPATHS=$($GO list -m -f {{.Dir}} all 2>/dev/null | grep "^$ROOTPATHPATTERN"\
-    | sed -e "s/^$ROOTPATHPATTERN//" -e 's/^\\//' -e 's/^\///')
-  MODPATHS=". $MODPATHS"
-  for module in $MODPATHS; do
-    echo "==> ${module}"
-    (cd $module && env GORACE='halt_on_error=1' CC=gcc $GO test -race ./...)
-  done
-
-  # check linters
-  if [[ $GOVERSION != 1.10 ]]; then
-    # linters do not work with modules yet
-    go mod vendor
-    unset GO111MODULE
-
-  fi
+  $GO build -v ./...
+  $GO test -v ./...
 
   echo "------------------------------------------"
   echo "Tests completed successfully!"
